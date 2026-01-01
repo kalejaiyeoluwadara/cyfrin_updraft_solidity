288e6e7af357a7f2e0f4fc1be39dff6c362f9402
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2026-01-01 19:44:45 +0100

    implemented library

--- FundMe.sol
+++ FundMe.sol
@@ -1,8 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 import {AggregatorV3Interface} from "@chainlink/contracts@1.5.0/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
 
 contract FundMe {
+    using PriceConverter for uint256;
+    
     uint256 public minimumUSD = 5e18;
     AggregatorV3Interface public priceFeed;
     address[] public funders;
@@ -16,20 +19,12 @@ contract FundMe {
 
     function fund() public payable {
         require(
-            getConversionRate(msg.value) >= minimumUSD,
+            msg.value.getConversionRate(priceFeed) >= minimumUSD,
             "Send at least $5 worth of ETH!"
         );
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] +=  msg.value;
     }
 
-    function getPrice() public view returns (uint256) {
-        (, int256 price,,,) = priceFeed.latestRoundData();
-        return uint256(price * 1e10); // convert 8 decimals → 18 decimals
-    }
-
-    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
-        uint256 ethPrice = getPrice();
-        return (ethPrice * ethAmount) / 1e18;
-    }
+  
 }
